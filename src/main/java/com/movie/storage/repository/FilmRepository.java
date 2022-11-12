353cac2e927d99f6d175d30832a98c03b98b2d57
Vladislav <vlad.shaldymov@mail.ru>
2022-11-12 16:25:16 +0500

    add data model

---
+++ src/main/java/com/movie/storage/repository/FilmRepository.java
@@ -0,0 +1,10 @@
+package com.movie.storage.repository;
+
+import com.movie.storage.model.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+
+}
